e69cb8411b143db32378bf4d9c99bda4d14bfae2
Kenni <kens123@hotmail.dk>
2020-03-05 12:44:15 +0100

    Add first simple graph using AndroidPlot

--- app/src/main/java/com/example/agraph/MainActivity.kt
+++ app/src/main/java/com/example/agraph/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.agraph
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.PointLabelFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
+import com.androidplot.util.PixelUtils
+import com.androidplot.xy.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +16,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         xyPlot = findViewById(R.id.plot)
-        
+
+        //List of values to display on y axis
+        var series1Numbers = listOf(1, 4, 2, 8, 4, 16, 8, 32, 16, 64)
+        var series2Numbers = listOf(5, 2, 10, 5, 20, 10, 40, 20, 80, 40)
+        //Set mock data for different series
+        var series1 = SimpleXYSeries(series1Numbers, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Folk under 50")
+        var series2 = SimpleXYSeries(series2Numbers, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Folk over 50")
+        //Set color for different series lines and dots.
+        var series1Format = LineAndPointFormatter(Color.RED, Color.GREEN, Color.TRANSPARENT, null)
+        var series2Format = LineAndPointFormatter(Color.BLUE, Color. RED, Color.TRANSPARENT, null)
+        // Sets the graphline as dashed. #Example of styling.
+        series2Format.linePaint.setPathEffect(DashPathEffect(floatArrayOf(
+            PixelUtils.dpToPix(20f),
+            PixelUtils.dpToPix(15f)), 0f))
+        //Adding the series to the plot display and changing label values.
+        xyPlot.addSeries(series1, series1Format)
+        xyPlot.addSeries(series2, series2Format)
+        xyPlot.title.text = "Folk med Gedeskæg"
+        xyPlot.domainTitle.text = "År det har groet"
+        xyPlot.rangeTitle.text = "Antal"
+
+
     }
 }
